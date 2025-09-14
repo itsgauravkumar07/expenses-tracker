230cd163172676276ac1fd8411f7c2a590e3bf2c
Gaurav kumar <01itsgauravpal@gmail.com>
2025-09-14 17:51:03 +0530

    feat: add validation in expense form

--- src/pages/AddExpense.jsx
+++ src/pages/AddExpense.jsx
@@ -1,29 +1,79 @@
 import { useState } from "react";
+import { addExpense } from "../services/localStorage";
 
 function AddExpense(){
 
-    const[expense, setExpense] = useState([]);
     const[name, setName] = useState("");
-    const[amount, setAmount] = useState(0);
-    const[date, setDate] = useState(null);
+    const[amount, setAmount] = useState("");
+    const[date, setDate] = useState("");
     const[category, setCategory] = useState("");
+
+    const[error, setError] = useState({
+        name: "",
+        amount: "",
+        date: "",
+        category: ""
+    });
+
+
+    const handleAdd = (e) => {
+
+        e.preventDefault();
+
+        if(name === ''){
+           setError(oldarr => ({...oldarr, name: "Name can't be empty"}));
+            return;
+        }
+
+        if(amount <= 0){
+           setError(oldarr => ({...oldarr, amount: "Enter valid amount"}));
+            return;
+        }
+
+        if(date === ''){
+          setError(oldarr => ({...oldarr, date: "Enter valid Date"}));
+           return;
+        }
+
+        if(category === ''){
+          setError(oldarr => ({...oldarr, category: "Enter valid Category"}));
+          return;
+        }
+
+        const expense = [{
+        id: Date.now(),
+        name: name,
+        amount: amount,
+        date: date,
+        category: category
+    }];
+
+    addExpense(expense);
+    alert("Expense added successfully");
+    }
+
+    
 
     return(
         <div>
-            <form>
-                <label>Expense name</label>
-                <input type="text" placeholder="e.g Movies, Fuel" value={name} onChange={(e) => setName(e.target.value)}/>
+            <form onSubmit={(e) => handleAdd(e)}>
+                <label>Expense name</label> <br />
+                <input type="text" placeholder="e.g Movies, Fuel" value={name} onChange={(e) => setName(e.target.value)}/> <br />
+                {error.name && <div>{error.name}</div>} <br />
 
-                <label>Amount</label>
-                <input type="number" placeholder="₹ 500" value={amount} onChange={(e) => setAmount(e.target.value)}/>
+                <label>Amount</label><br />
+                <input type="number" placeholder="₹ 500" value={amount} onChange={(e) => setAmount(e.target.value)}/> <br />
+                {error && <div>{error.amount}</div>} <br />
 
-                <label>Category</label>
-                <input type="text" placeholder="e.g food, shopping" value={category} onChange={(e) => setCategory(e.target.value)}/>
+                <label>Category</label><br />
+                <input type="text" placeholder="e.g food, shopping" value={category} onChange={(e) => setCategory(e.target.value)}/> <br />
+                {error && <div>{error.category}</div>} <br />
                 
-                <label>Date</label>
-                <input type="date" value={date} onChange={(e) => setDate(e.target.value)}/>
+                <label>Date</label><br />
+                <input type="date" value={date} onChange={(e) => setDate(e.target.value)}/> <br />
+                {error && <div>{error.date}</div>} <br />
 
-                <button onSubmit={handleAdd()}>Add Expense</button>
+                <button type="submit">Add Expense</button>
             </form>
 
         </div>
