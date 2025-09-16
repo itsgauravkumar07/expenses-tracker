ca834c9e582c58da01c03d0c0b7e68cc0a960aaa
Gaurav kumar <01itsgauravpal@gmail.com>
2025-09-16 16:38:38 +0530

    feat: add custom react-toastify toast notifer when user click clear all button

--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -1,9 +1,10 @@
 import { deleteAllExpense } from "../services/localStorage";
-
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Setting(){
 
-    
+    const notify = () => toast.error("All Expenses is deleted", { autoClose: 2000 });
 
     const handleDelete = () => {
         deleteAllExpense();
@@ -13,7 +14,7 @@ function Setting(){
         <div>
             <p>Clear all data</p>
             <button onClick={handleDelete}>Clear all</button>
-            
+            <ToastContainer position = "top-right" />
         </div>
     )
 }
